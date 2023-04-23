782ada96f7945518f960942ae712ab3aba17b953
ifleeyo180 <ifleeyo180@gmail.com>
2023-04-23 18:29:00 +0800

    fix

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ pip install -r requirements.txt
 
 if [[ $CREATE_SUPERUSER ]];
 then
-  python world_champ_2022/manage.py createsuperuser --no-input
+  python manage.py createsuperuser --no-input
 fi
