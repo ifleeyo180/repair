47804f8358c53b6a62807868f831fb0499368e4a
ifleeyo180 <ifleeyo180@gmail.com>
2023-04-23 18:33:15 +0800

    fix

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ pip install --upgrade pip
 
 pip install -r requirements.txt
 
+python manage.py makemigrations
+python manage.py migrate
+
+
 if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py createsuperuser --no-input
