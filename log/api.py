45561a0571b46bf522c891c7ed3eab55d5b79cd0
ifleeyo180 <ifleeyo180@gmail.com>
2023-04-24 23:46:05 +0800

    feat logitems

--- log/api.py
+++ log/api.py
@@ -1,6 +1,8 @@
-from typing import List
+from typing import List, Optional
 from ninja import NinjaAPI, Schema
-from .models import Tag
+from .models import Tag, LogItem
+from django.db.models import Q
+
 
 api = NinjaAPI()
 
@@ -9,7 +11,49 @@ class TagSchema(Schema):
     Rstyle: str
 
 
+class LogItemSchema(Schema):
+    id:int
+    ctime: str
+    subject: str
+    description: str
+    reporter: str
+    phone: str
+    handler: str
+    status: int
+    utime: str
+    comment: str
+    picture: Optional[str]
+    tags: List[TagSchema]
+
+
+def logitem_to_schema(logitem: LogItem) -> LogItemSchema:
+    return LogItemSchema(
+        id=logitem.id,
+        ctime=logitem.ctime.strftime('%Y-%m-%d %H:%M:%S'),
+        subject=logitem.subject,
+        description=logitem.description,
+        reporter=logitem.reporter,
+        phone=logitem.phone,
+        handler=logitem.handler,
+        status=logitem.status,
+        utime=logitem.utime.strftime('%Y-%m-%d %H:%M:%S'),
+        comment=logitem.comment,
+        picture=logitem.picture.url if logitem.picture else None,
+        tags=[TagSchema(subject=tag.subject, Rstyle=tag.Rstyle) for tag in logitem.tags.all()],
+    )
+
+
+
+
 @api.get("tags", response=List[TagSchema])
 def tags(request):
     # tags = Tag.objects.all()
     return [TagSchema(subject=tag.subject, Rstyle=tag.Rstyle) for tag in Tag.objects.all()]
+
+
+@api.get("logitems", response=List[LogItemSchema])
+def logitems(request):
+    logitems = LogItem.objects.all()
+    return [logitem_to_schema(logitem) for logitem in logitems]
+
+
