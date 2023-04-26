869f7897774ec67e4fbe83bf9cd8a76c2f56f404
ifleeyo180 <ifleeyo180@gmail.com>
2023-04-26 22:58:28 +0800

    fix js 刪除功能

--- log/urls.py
+++ log/urls.py
@@ -15,5 +15,8 @@ from .api import api
 urlpatterns = [
     path('', index, name='log_list'),
     path('create/', LogCreate.as_view(), name='log_create'),
+    path('<int:pk>/', LogView.as_view(), name='log_view'),
+    path('<int:pk>/reply/', LogReply.as_view(), name='log_reply'),
+    path('<int:pk>/delete/', LogDelete.as_view(), name='log_delete'),
     path('api/', api.urls),
 ]
